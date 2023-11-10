ba0e0e94e4fdf88c70d5f9099cebe9b708510720
Fio_User <arsenijbitkin199@gmail.com>
2023-11-10 18:11:26 +0700

    My first git-repo

--- 1.py
+++ 1.py
@@ -1,20 +1,56 @@
 import sys
+import io
 
+import random
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
 
+template = """<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>Form</class>
+ <widget class="QWidget" name="Form">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>500</width>
+    <height>382</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Form</string>
+  </property>
+  <widget class="QPushButton" name="pushButton">
+   <property name="geometry">
+    <rect>
+     <x>130</x>
+     <y>110</y>
+     <width>221</width>
+     <height>121</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Кнопка</string>
+   </property>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+"""
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
 
     def initUI(self):
+        self.do_paint = False
         self.setGeometry(300, 300, 200, 200)
         self.setWindowTitle('Рисование')
 
-        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -30,8 +66,11 @@ class Example(QWidget):
         self.update()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 0, 0))
-        qp.drawEllips(30, 30, 120, 30)
+        qp.setBrush(QColor(random.randrange(256), random.randrange(256), random.randrange(256)))
+        a = random.randint(50, 150)
+        x = random.randint(0, 200)
+        y = random.randint(0, 200)
+        qp.drawEllipse(y, x, a, a)
 
 
 if __name__ == '__main__':
